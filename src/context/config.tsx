329d98464098f254b8c5ce0114015fba87f1d692
pkaplan <pkaplan1@seznam.cz>
2021-10-12 20:20:27 +0200

    git cache cleared

--- src/context/config.tsx
+++
@@ -1,3 +0,0 @@
-export const config = {
-  headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` },
-};
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import App from "./App";
-import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-reportWebVitals();
